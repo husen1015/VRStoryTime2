e647fd13e1fee9b0276e5e64d855dfbd1b0c24d1
Husen Abdallah <Husen.ab1015@gmail.com>
2023-06-30 10:24:00 +0300

    book hovering now activates when player points hand towards book

--- VRStoryTimeSemesterB/Assets/Scripts/bookButton.cs
+++ VRStoryTimeSemesterB/Assets/Scripts/bookButton.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class bookButton : MonoBehaviour
 {
     public GameObject book;
+    public Transform player;
+    float weight = 0.9f;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
+    }
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(book.transform.position, player.position, weight); ;
+        transform.LookAt(player.position);
+    }
     public void activateBook()
     {
-        book.GetComponent<BookBehavior>().shouldMove= true;
+        book.GetComponent<BookBehavior>().shouldMove = true;
+        Destroy(gameObject);
     }
 }
